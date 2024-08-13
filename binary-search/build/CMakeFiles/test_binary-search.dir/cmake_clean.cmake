d148091b29b51f3496aa3ad55d8210c0b4a45416
chinepun <chokejiri@gmail.com>
2024-08-13 18:23:22 +0100

    Actually removed build folders this time

--- binary-search/build/CMakeFiles/test_binary-search.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_binary-search"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_binary-search.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
